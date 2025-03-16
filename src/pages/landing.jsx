4279a24e03f055ff714a80e3e1dc051508411c2a
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-03-15 22:57:53 -0400

    add custom made logo

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,6 +4,7 @@ import 'materialize-css/dist/js/materialize.min.js'; //importing Materialize's J
 import '../../public/styles/landing.css'; //importing this page's custom styling
 import Footer from "../components/footer";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png"; //importing application's brand's logo
 
 const Landing = () => {
     const navigate = useNavigate();
@@ -13,8 +14,9 @@ const Landing = () => {
                 <div className="row">
                     <div className="col s12">
                         {/* Application's logo goes here */}
-                        <h1 className="rubik-semibold">Welcome to BlogHive</h1>
-                        <h2 className="rubik-normal">Where you can share your thoughts</h2>
+                        <div>
+                            <img src={logo} alt="BlogHive" id="bloghive"/>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
