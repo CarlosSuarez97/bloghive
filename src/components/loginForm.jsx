f58738ab0131947303e5112bdb4bc4cd37e5bb00
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-03-16 20:49:18 -0400

    tweaked more design stuff

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -10,24 +10,24 @@ const LoginForm = () => {
                 <div className="row">
                     <div className="col s12">
                         <div className="card">
-                        <div className="card-content black-text">
-                            <span className="card-title"><h1>Log into your account</h1></span>
+                        <div className="card-content white-text formCard">
+                            <span className="card-title rubik-medium"><h1>Log into your account</h1></span>
                             <div className="row">
                                 <form action="" className="col s12" method="post">
                                     <div className="row">
                                         <div className="input-field col s12">
-                                            <input type="email" id="userEmail" name="userEmail" class="validate black-text"/>
-                                            <label htmlFor="userEmail">Email address</label>
+                                            <input type="email" id="userEmail" name="userEmail" class="validate white-text"/>
+                                            <label htmlFor="userEmail" className="white-text">Email address</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s12">
-                                            <input type="password" name="userPassword" id="userPassword" class="validate black-text"/>
-                                            <label htmlFor="userPassword">Password</label>
+                                            <input type="password" name="userPassword" id="userPassword" class="validate white-text"/>
+                                            <label htmlFor="userPassword" className="white-text">Password</label>
                                         </div>
                                     </div>
                                     <div className="row">
-                                        <button type="submit" name="createUser" className="btn btn-large waves-effect waves-light">Log In</button>
+                                        <button type="submit" name="createUser" className="btn btn-large waves-effect waves-light" id="submitButton">Log In</button>
                                     </div>
                                 </form>
                             </div>
--- src/components/signUpForm.jsx
+++ src/components/signUpForm.jsx
@@ -10,36 +10,36 @@ const SignUpForm = () => {
                 <div className="row">
                     <div className="col s12">
                         <div className="card">
-                        <div className="card-content black-text">
-                            <span className="card-title"><h1>Create an account</h1></span>
+                        <div className="card-content white-text formCard">
+                            <span className="card-title rubik-medium"><h1>Create an account</h1></span>
                             <div className="row">
                                 <form action="" className="col s12" method="post">
                                     <div className="row">
                                         <div className="input-field col s12">
-                                            <input type="text" id="fullName" name="userFullName" class="validate black-text" />
-                                            <label htmlFor="fullName">Full name</label>
+                                            <input type="text" id="fullName" name="userFullName" class="validate white-text" />
+                                            <label htmlFor="fullName" className="white-text">Full name</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s12">
-                                            <input type="email" id="userEmail" name="userEmail" class="validate black-text"/>
-                                            <label htmlFor="userEmail">Email address</label>
+                                            <input type="email" id="userEmail" name="userEmail" class="validate white-text"/>
+                                            <label htmlFor="userEmail" className="white-text">Email address</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s12">
-                                            <input type="password" name="userPassword" id="userPassword" class="validate black-text" />
-                                            <label htmlFor="userPassword">Password</label>
+                                            <input type="password" name="userPassword" id="userPassword" class="validate white-text" />
+                                            <label htmlFor="userPassword" className="white-text">Password</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s12">
-                                            <input type="password" name="userConfirmPass" id="passwordConfirm" class="validate black-text" />
-                                            <label htmlFor="passwordConfirm">Confirm password</label>
+                                            <input type="password" name="userConfirmPass" id="passwordConfirm" class="validate white-text" />
+                                            <label htmlFor="passwordConfirm" className="white-text">Confirm password</label>
                                         </div>
                                     </div>
                                     <div className="row">
-                                        <button type="submit" name="createUser" className="btn btn-large waves-effect waves-light">Sign Up</button>
+                                        <button type="submit" name="createUser" className="white-text btn btn-large waves-effect waves-light" id="submitButton">Sign Up</button>
                                     </div>
                                 </form>
                             </div>
