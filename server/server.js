7b6da341b9c7c30ef5e4b536ead5319afc84b82b
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-03-24 17:29:23 -0400

    Install missing dependencies for the server

--- server/server.js
+++ server/server.js
@@ -4,3 +4,31 @@ import express from "express";
 import morgan from "morgan";
 import pg from "pg";
 import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // loading environment variables
+
+const app = express();
+const port = process.env.port || 3000;
+const db = new pg.Client({
+    database: "bloghive",
+    password: "1234",
+    user: "postgres",
+    host: "localhost",
+    port: 5432
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({extended: true})); // parse url-encoded data
+app.use(bodyParser.json()); // parse JSON request bodies
+app.use(cors()); // allow frontend requests
+
+app.get("/", (req, res) => {
+    res.send("Server running");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
