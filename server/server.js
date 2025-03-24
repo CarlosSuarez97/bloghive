609b7d8c3e234c5d02963b7c0a06f0da1c7d1a0d
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-03-24 17:00:33 -0400

    Install missing dependencies

---
+++ server/server.js
@@ -0,0 +1,6 @@
+//This is the Javascript file where the server for the application will be made
+import bodyParser from "body-parser";
+import express from "express";
+import morgan from "morgan";
+import pg from "pg";
+import axios from "axios";
