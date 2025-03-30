917ac7c379cba6616d814c9b7c15ccbfa21e6dbb
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-03-29 23:21:22 -0400

    User creation and authentication. Signing up works well, have got to work a bit harder on improving the login in one

--- server/server.js
+++ server/server.js
@@ -3,34 +3,89 @@ import bodyParser from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import pg from "pg";
-import axios from "axios";
-import cors from "cors";
-import dotenv from "dotenv";
+import cors from "cors"; // to allow requests from the React frontend
+import dotenv from "dotenv"; // for the database credentials
 import bcrypt from "bcrypt"; //for encrypting and hashing passwords entered by the user on the client side
 
 
 dotenv.config(); // loading environment variables
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const db = new pg.Client({
-    database: "bloghive",
-    password: "1234",
-    user: "postgres",
-    host: "localhost",
-    port: 5432
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 });
 const saltRounds = 10;
 
 db.connect();
 
-app.use(bodyParser.urlencoded({extended: true})); // parse url-encoded data
-app.use(bodyParser.json()); // parse JSON request bodies
-app.use(cors()); // allow frontend requests
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
+//Creating new users
 app.post("/signup", async (req, res) => {
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
+    const {
+        email,
+        password,
+        firstName,
+        lastName
+    } = req.body;
+    try {
+        const checkResult = await db.query("SELECT * FROM user_info WHERE user_email = $1", [email]);
+        if(checkResult.rows.length > 0) {
+            console.log("There's an user registered with this email already.");
+        } else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if(err) {
+                    console.log("There's been an issue: ", err);
+                } else {
+                    const result = await db.query("INSERT INTO user_info (user_email, user_password, user_first_name, user_last_name) VALUES ($1, $2, $3, $4)", [email, hash, firstName, lastName]);
+                    console.log(result);
+                }
+            });
+        }
+    } catch (error) {
+        console.log("There's been an issue: ", error);
+    }
+
+    
+});
+
+//Authenticating users when logging in
+app.post("/login", async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+    try {
+        const result = await db.query("SELECT * FROM user_info WHERE user_email = $1", [email]);
+        if(result.rows.length > 0) {
+            //Comparing the hashed password stored in the database against what the user's entered
+            const user = result.rows[0];
+            const storedPassword = user.password;
+            bcrypt.compare(password, storedPassword, (err, result) => {
+                if (err) {
+                    console.log("There's been an issue: ", err);
+                } else {
+                    if(result) {
+                        console.log("You've logged in successfully");
+                    } else {
+                        console.log("The information provided doesn't match anything in our records.");
+                    }
+                }
+            });
+        } else {
+            console.log("The information provided doesn't match anything in our records.");
+        }
+    } catch (err) {
+        console.log("There's been an issue: ", err);
+    }
 })
 
 app.listen(port, () => {
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,22 +1,45 @@
 import React, {useState} from "react";
+import axios from "axios";
 import 'materialize-css/dist/css/materialize.min.css'; //importing Materialize CSS
 import 'materialize-css/dist/js/materialize.min.js'; //importing Materialize's JS components
-import "../../public/styles/loginForm.css"
+import "../../public/styles/loginForm.css"; //importing the custom CSS file for this component
+
+const serverURL = "http://localhost:3000";
 
 const LoginForm = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
     const [showPassword, setShowPassword] = useState(false);
+    
+    const handleLogin = async (e) => {
+
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(serverURL + "/login", {
+                email,
+                password
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.log("There's been an issue: ", error);
+        }
+
+    }
+
     return(
         <div className="container">
-            <form action="" className="col s12">
+            <form className="col s12" onSubmit={handleLogin}>
                 <div className="row">
                     <div className="input-field col s12">
-                        <input type="email" name="email" id="email" className="validate"/>
+                        <input type="email" value={email} id="email" className="validate" onChange={(e) => setEmail(e.target.value)}/>
                         <label htmlFor="email">Email</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col s12">
-                        <input type={showPassword ? "text": "password"} name="password" id="password"/>
+                        <input type={showPassword ? "text": "password"} name="password" value={password} id="password" onChange={(e) => setPassword(e.target.value)}/>
                         <label htmlFor="password">Password</label>
                         <span
                         className="material-icons prefix"
